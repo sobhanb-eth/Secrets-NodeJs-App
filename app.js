677f99f245609addb63c1c89109264f6ccea3c36
Sobhan Bahrami <sobhan.bahrami@metu.edu.tr>
2020-02-07 01:15:44 +0200

    add google OAuth2.0, social btn css, submit secret to DB and code refactor

--- app.js
+++ app.js
@@ -7,16 +7,20 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+
 
 const app = express();
 
-console.log(process.env.API_KEY);
+
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
   extended: true
 }));
+
 
 //session
 app.use(session({
@@ -29,6 +33,8 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 
+
+//DB Connection string
 mongoose.connect("mongodb://localhost:27017/userDB", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -38,33 +44,72 @@ mongoose.connect("mongodb://localhost:27017/userDB", {
   .then((err) => console.log("MongoDB Connected. . ."))
   .catch((err) => console.log(err));
 
-//Schema
+
+
+//DB Table Schema
 
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+  secret: String
 });
 
-//Plugin
+
+//Mongoose Plugin for hash and salt (password)
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
-//Model
+
+//Mongoose Model
 
 const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
 });
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/secrets",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    console.log(profile);
+
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
+
+
 
 app.get("/", function(req, res) {
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate('google', { scope: ["profile"] }));
+
+app.get("/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect("/secrets");
+  });
 
 app.get("/login", function(req, res) {
   res.render("login");
@@ -81,6 +126,35 @@ app.get("/secrets", function(req, res) {
     res.redirect("/login");
   }
 });
+
+
+app.get("/submit", function (req, res) {
+  if (req.isAuthenticated()) {
+    res.render("submit");
+  } else {
+    res.redirect("/login");
+  }
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+
+  console.log(req.user.id);
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
+});
+
 
 app.get("/logout", function (req, res) {
   req.logout();
@@ -116,4 +190,11 @@ app.post("/login", function(req, res) {
     }
   });
 
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("Server started on port 3000");
 });
